ab7beca5e65f61a5100c09006d4f50a8fcf281a4
xinshang1215 <94335897@qq.com>
2020-11-04 15:08:14 +0800

    更换

--- src/main.js
+++ src/main.js
@@ -2,15 +2,18 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import Axios from 'axios'
+import vueAxios from 'vue-axios'
 
 Vue.config.productionTip = false
 
+Vue.use(vueAxios,Axios)
 //导航守卫，全局前置守卫
-router.beforeEach((to,from,next)=>{
-  let token = 
-  if(to.name !== "Login" && to.isAuth && isLogined) next({name:"Login"})
-  else next()
-})
+// router.beforeEach((to,from,next)=>{
+//   let token = 
+//   if(to.name !== "Login" && to.isAuth && isLogined) next({name:"Login"})
+//   else next()
+// })
 
 new Vue({
   router,
